9f9440add7f70ea1c77f6aff63e7557e68401d0d
Core i7 <Core i7@DESKTOP-R50JOH6>
2021-03-24 22:05:35 +0900

    updated composecoffee.java

--- src/ComposeCoffee.java
+++ src/ComposeCoffee.java
@@ -14,38 +14,72 @@ public class ComposeCoffee {
 			System.out.println("5. Show a menu");
 			System.out.println("6. Exit");
 			System.out.println("Select one number between 1-6: ");
-			num=input.nextInt();
-			switch(num) {
-			case 1:
-				System.out.println("Product Number: ");
-				int productnum1 = input.nextInt();
-				System.out.println("Product Name: ");
-				String productname = input.next();
-				break;
-				
-			case 2:
-				System.out.println("Write the product number you want to delete.");
-				System.out.println("Product Number: ");
-				int productnum2 = input.nextInt();
-				break;
-				
-			case 3:
-				System.out.println("Write the product number you want to edit.");
-				System.out.println("Product Number: ");
-				int productnum3 = input.nextInt();
-				System.out.println("Write the product name you want to change value.");
-				String productnum4 = input.next();
-				break;
-			case 4:
-				System.out.println("Write the product number you want to view.");
-				System.out.println("Product Number: ");
-				int productnum5 = input.nextInt();
-				break;
-							
-				
+			
+			
+		while ((num = input.nextInt()) !=6 ) {
+			switch (num) {
+				case (1) : addProduct(); break;
+				case (2) : deleteProduct(); break;
+				case (3) : editProduct(); break;
+				case (4) : viewProduct(); break;
+				case (5) : printmenu(); break;
+				default :
+					System.out.println("메뉴를 잘못 선택하셨습니다." + "\n");
+				}
 			}
-		
+				
 		}
+	}
+	
+	
+	
+	static public void addProduct() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Product Number: ");
+		int productnum1 = input.nextInt();
+		System.out.println("Product Name: ");
+		String productname = input.next();
+		
+		printmenu();
+	}
+		
+	static public void deleteProduct() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Write the product number you want to delete.");
+		System.out.println("Product Number: ");
+		int productnum2 = input.nextInt();
+		
+		printmenu();
+	}
+		
+	static public void editProduct() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Write the product number you want to edit.");
+		System.out.println("Product Number: ");
+		int productnum3 = input.nextInt();
+		System.out.println("Write the product name you want to change value.");
+		String productnum4 = input.next();
+		
+		printmenu();
+	}
+		
+	static public void viewProduct() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Write the product number you want to view.");
+		System.out.println("Product Number: ");
+		int productnum5 = input.nextInt();
+		
+		printmenu();
+	}
+	
+	static public void printmenu() {
+		System.out.println("1. Add Products");
+		System.out.println("2. Delete Products");
+		System.out.println("3. Edit Products");
+		System.out.println("4. View Products");
+		System.out.println("5. Show a menu");
+		System.out.println("6. Exit");
+		System.out.println("Select one number between 1-6: ");
 	}
 
 }
