8200dce5a4bc334ec91470f7978b34df86ca27fa
Core i7 <Core i7@DESKTOP-R50JOH6>
2021-04-07 19:55:28 +0900

    added juicecoffee

---
+++ src/Coffee/JuiceCoffee.java
@@ -0,0 +1,35 @@
+package Coffee;
+
+import java.util.Scanner;
+
+public class JuiceCoffee extends Coffee {
+	public void getCoffeeInput(Scanner input) {
+		System.out.println("Product Number: ");
+		int Pnum = input.nextInt();
+		this.setPnum(Pnum);
+	
+		System.out.println("Product Name: ");
+		String Pname = input.next();
+		this.setPname(Pname);
+		
+		char answer = 'x';
+		while (answer != 'y' || answer != 'Y' || answer != 'n' || answer != 'N') {
+			System.out.println("Do you want to ice? (Y/N)");
+			answer = input.next().charAt(0);
+			if (answer == 'y' || answer == 'Y') {
+				System.out.println("Product Price: ");
+				int price = input.nextInt() + 500;
+				this.setPrice(price);
+				break;
+			}
+			else if (answer == 'n' || answer == 'N') {
+				System.out.println("Product Price: ");
+				int price = input.nextInt();
+				this.setPrice(price);
+				break;
+			}
+			else {
+			}
+		}
+	}
+}
